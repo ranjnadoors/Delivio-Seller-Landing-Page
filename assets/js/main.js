5c571521e8aabb61a5798163477b1f5342b50016
ranjna <ranjna@doors4s.com>
2021-08-24 15:21:26 +0530

    kk

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,15 +1,20 @@
-$(document).ready(function () {
+/*****$(document).ready(function () {
+    const checkboxes = [];
     $('.myImage').on("click", function () {
         var checked = $(this).next('.myHiddenCheckbox').prop('checked');
+        
+       
         if (checked == false) {
+           
             $('.myImage').next('.myHiddenCheckbox').attr("checked", true)
             $(this).attr("src", "seller-img/Group 252.png");
         } else {
+                                   
             $('.myImage').next('.myHiddenCheckbox').attr("checked", false)
             $(this).attr("src", "seller-img/Group 251.png");
         }
     });
-});
+});************/
 
 
 
